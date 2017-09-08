41030b92ec3e2c3fdedd138da184b9aa351379ed
toluwajosh <tjosh.owoyemi@gmail.com>
2017-09-08 11:34:28 +0900

    fix: fixed non matching size in layers

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def load_vgg(sess, vgg_path):
     
     return image_input, keep_prob, layer3_out, layer4_out, layer7_out
 
-# tests.test_load_vgg(load_vgg, tf)
+tests.test_load_vgg(load_vgg, tf)
 
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
@@ -58,36 +58,49 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, strides=(1,1), padding='same', 
+    conv7 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, strides=(1,1), padding='same', 
                                 kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    # print('\nsize of conv7:\n')
+    # print_out_ = tf.Print(conv7, [tf.shape(conv7)])
+    # print(print_out_)
+
+    conv4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, strides=(1,1), padding='same', 
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    # print('\nsize of conv4:\n')
+    # print_out_ = tf.Print(conv4, [tf.shape(conv4)])
+    # print(print_out_)
+
+    conv3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, strides=(1,1), padding='same', 
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    # print('\nsize of conv3:\n')
+    # print_out_ = tf.Print(conv3, [tf.shape(conv3)])
+    # print(print_out_)
+
     # upsample by 2
-    upsamp_2_1 = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, strides=(2, 2), padding='same', 
+    upsamp_32x = tf.layers.conv2d_transpose(conv7, num_classes, 4, strides=(2, 2), padding='same', 
                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    vgg_layer7_out = tf.reshape(vgg_layer7_out, (-1, num_classes)) # reshape input
-    skip_add_1 = tf.add(upsamp_2_1, vgg_layer7_out)
-    # upsample by 2
-    upsamp_2_2 = tf.layers.conv2d_transpose(skip_add_1, num_classes, 4, strides=(2, 2), padding='same', 
-                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    vgg_layer4_out = tf.reshape(vgg_layer4_out, (-1, num_classes)) 
-    skip_add_2 = tf.add(upsamp_2_2, vgg_layer4_out)
-
-    # upsample by 8
-    output = tf.layers.conv2d_transpose(upsamp_2_2, num_classes, 16, strides=(8, 8), padding='same', 
-                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    vgg_layer3_out = tf.reshape(vgg_layer3_out, (-1, num_classes)) 
-    output = tf.add(output, vgg_layer3_out)
 
 
-    # to know the output of the layers
-    #print('\noutput here:\n')
-    #print_out_ = tf.Print(output, [tf.shape(output)])
-    #print(print_out_)
+    # add skip connection and upsample by 2
+    skip_add_1 = tf.add(upsamp_32x, conv4)
+    upsamp_16x = tf.layers.conv2d_transpose(skip_add_1, num_classes, 4, strides=(2, 2), padding='same', 
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+
+    # add skip connection and upsample by 8
+    skip_add_2 = tf.add(upsamp_16x, conv3)
+    output = tf.layers.conv2d_transpose(skip_add_2, num_classes, 16, strides=(8, 8), padding='same', 
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
     # use the right padding and reg
     # upsample by 2, 2, and 8
     # return final output which is the same size as the image
     return output
 
-# tests.test_layers(layers)
+tests.test_layers(layers)
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
@@ -120,7 +133,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
 
     return logits, train_op, cross_entropy_loss
 
-# tests.test_optimize(optimize)
+tests.test_optimize(optimize)
 
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, 
@@ -150,7 +163,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op,
 
         print("epoch: {}, batch: {}, loss: {}".format(epoch, i, loss))
       
-# tests.test_train_nn(train_nn)
+tests.test_train_nn(train_nn)
 
 
 def run():
@@ -168,7 +181,7 @@ def run():
     #  https://www.cityscapes-dataset.com/
 
     epochs = 20
-    batches = 2
+    batches = 4
 
     with tf.Session() as sess:
         # Path to vgg model
@@ -181,13 +194,10 @@ def run():
 
         # TODO: Build NN using load_vgg, layers, and optimize function
         input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
-        layer = layers(layer3_out, layer4_out, layer7_out, num_classes)
+        final_layer = layers(layer3_out, layer4_out, layer7_out, num_classes)
         label = tf.placeholder(tf.int32, shape=[None, None, None, num_classes])
         learning_rate = tf.placeholder(tf.float32)
-        logits, train_op, loss = optimize(layer, label, learning_rate, num_classes)
-        #final_layer = layers(layer3_out, layer4_out, layer7_out, num_classes)
-
-        
+        logits, train_op, loss = optimize(final_layer, label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
         sess.run(tf.global_variables_initializer())
